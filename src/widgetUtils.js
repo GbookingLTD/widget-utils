ea3d479e6a094e1569b390e728f7abf20af9a081
olzhas <olzhas@gbooking.ru>
2016-05-23 13:00:24 +0600

    Added roundNumberUsingRule

--- src/widgetUtils.js
+++ src/widgetUtils.js
@@ -1,7 +1,9 @@
 import * as DateTime from './dateTime';
 import * as BusySlots from './busySlots';
+import * as roundNumberUsingRule from './roundNumberUsingRule';
 
 export default {
   DateTime: DateTime,
-  BusySlots: BusySlots
+  BusySlots: BusySlots,
+  roundNumberUsingRule
 };
