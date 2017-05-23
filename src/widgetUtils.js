8b39700aa50cb1cfb863b83c16e9e0e1758feb04
Vasiliy Os <talk@vasiliy0s.com>
2017-05-23 14:30:52 +0300

    Add WidgetUtils.Crac.toBusySlots()

--- src/widgetUtils.js
+++ src/widgetUtils.js
@@ -1,11 +1,13 @@
 import * as DateTime from './dateTime';
 import * as BusySlots from './busySlots';
 import * as Booking from './booking';
+import * as Crac from './crac';
 import { roundNumberUsingRule } from './roundNumberUsingRule';
 
 export default {
-  DateTime: DateTime,
-  BusySlots: BusySlots,
-  Booking: Booking,
-  roundNumberUsingRule: roundNumberUsingRule
+  DateTime,
+  BusySlots,
+  Booking,
+  Crac,
+  roundNumberUsingRule,
 };
