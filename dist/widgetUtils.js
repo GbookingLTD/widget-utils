f84a985ab8ce8caa131b717167e0d8b54817bad7
Alexander Jukov <alex.zhukov@gbooking.ru>
2018-01-18 15:00:12 +0500

    taxonomy children type support

--- dist/widgetUtils.js
+++ dist/widgetUtils.js
@@ -1504,6 +1504,73 @@ var langUtils = Object.freeze({
     getCountryLang: getCountryLang
   });
 
+  var TAXONOMY_CHILDREN = 'CHILDREN';
+  var TAXONOMY_ADULT = 'PARENT';
+  var TAXONOMY_COMMON = 'COMMON';
+  var TAXONOMY_BOTH = 'BOTH';
+
+  function setChildTaxonomies(taxonomies, resources) {
+    var strictInclusion = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+
+    var childOnly = {};
+    var adultOnly = {};
+
+    if (!Array.isArray(taxonomies) || !Array.isArray(resources)) {
+      console.log('empty data');
+      return taxonomies;
+    }
+
+    childOnly = getTaxonomiesType(resources, true);
+    adultOnly = getTaxonomiesType(resources, false);
+
+    taxonomies.forEach(function (t) {
+      t.childrenType = getTaxonomyType(childOnly, adultOnly, parseInt(t.taxonomyId), strictInclusion);
+    });
+    return taxonomies;
+  };
+
+  function getTaxonomiesType(resources) {
+    var isChild = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+
+    var T = {};
+    resources.forEach(function (r) {
+      if (r.taxonomyChildren && r.taxonomyChildren.length > 0) {
+        r.taxonomyChildren.forEach(function (c) {
+          if (c.children === isChild) {
+            T[parseInt(c.taxonomyID)] = true;
+          }
+        });
+      }
+    });
+    return T;
+  }
+
+  function getTaxonomyType(C, P, taxonomyID, strictInclusion) {
+    if (strictInclusion) {
+      if (C[taxonomyID] && !P[taxonomyID]) {
+        return TAXONOMY_CHILDREN;
+      } else if (!C[taxonomyID] && P[taxonomyID]) {
+        return TAXONOMY_ADULT;
+      } else if (C[taxonomyID] && P[taxonomyID]) {
+        return TAXONOMY_BOTH;
+      }
+      return TAXONOMY_COMMON;
+    } else {
+      if (C[taxonomyID] && P[taxonomyID]) {
+        return TAXONOMY_BOTH;
+      } else if (!C[taxonomyID] && !P[taxonomyID]) {
+        return TAXONOMY_COMMON;
+      } else if (C[taxonomyID]) {
+        return TAXONOMY_CHILDREN;
+      }
+      return TAXONOMY_ADULT;
+    }
+  }
+
+var taxonomies = Object.freeze({
+    setChildTaxonomies: setChildTaxonomies
+  });
+
   var widgetUtils = {
     DateTime: DateTime,
     BusySlots: BusySlots,
@@ -1511,7 +1578,8 @@ var langUtils = Object.freeze({
     Crac: Crac,
     roundNumberUsingRule: roundNumberUsingRule,
     phoneUtils: phoneUtils,
-    langUtils: langUtils
+    langUtils: langUtils,
+    taxonomies: taxonomies
   };
 
   return widgetUtils;
