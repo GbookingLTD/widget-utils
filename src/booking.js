da691677b82ec7fbca0601e2ec41089f7eb7a371
Alexander Jukov <alex.zhukov@gbooking.ru>
2018-03-16 17:48:04 +0500

    Support for GT records without time selection

--- src/booking.js
+++ src/booking.js
@@ -13,10 +13,13 @@ import { isDateForbidden, checkDate } from './busySlots';
  * @param day
  * @returns {boolean}
  */
-export function calendarBookingTime(businessData, busySlots, slotSize, day) {
+export function calendarBookingTime(businessData, busySlots, slotSize, day, isGT) {
   var widgetConfiguration = businessData.business.widget_configuration;
   if (isDateForbidden(widgetConfiguration, day.date)) {
     return;
+  }
+  if(isGT){
+    return calendarBookingTimeGT(businessData, busySlots, slotSize, day);
   }
   var slotDay = _(busySlots.days).find(function (d) {
     return moment(d.date).isSame(day.date, 'day');
@@ -43,6 +46,26 @@ export function calendarBookingTime(businessData, busySlots, slotSize, day) {
       }
       startTime.setUTCMinutes(startTime.getMinutes() + dateCheck[1]);
     }
+  }
+}
+
+function calendarBookingTimeGT(businessData, slots, slotSize, day) {
+
+  var slotDay = _(slots.days).find(function (d) {
+    return moment(d.date).isSame(day.date, 'day');
+  });
+  var selectedSlot = undefined;
+  if (slotDay && slotDay.slots && slotDay.slots.length > 0) {
+    for (var i = 0; i < slotDay.slots.length; i++) {
+      if (slotDay.slots[i].space_left > 0) {
+        var checkSlot = moment.utc(slotDay.date).add(slotDay.slots[i].time, 'm');
+        if(checkSlot > moment.utc()){
+          selectedSlot = checkSlot;
+          break;
+        }
+      }
+    }
+    return selectedSlot;
   }
 }
 
