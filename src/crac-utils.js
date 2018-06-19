08b0400797afbc4e9ba04d43c99f6977a8a79056
swvitaliy <sw.vitaliy@gmail.com>
2018-06-19 16:12:19 +0300

    fix calcCRACSlotIntermediate

--- src/crac-utils.js
+++ src/crac-utils.js
@@ -11,7 +11,7 @@ import {setAnd, setUnion} from './crac';
 // Remove this function after migration
 function calcCRACSlotIntermediate(slot, vectorSlotSize) {
   return slot.resources.reduce((ret, res) => {
-    let bitset = setAnd(res.bitset, res.taxonomyBitSet);
+    let bitset = res.taxonomyBitSet ? setAnd(res.bitset, res.taxonomyBitSet) : res.bitset;
     return setUnion(ret, bitset);
   }, '0'.repeat(vectorSlotSize === 5 ? 288 : 1440).split('').map(() => 0));
 }
